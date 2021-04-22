1609bec91ae61eb391ce8e094f7bd39b62a65b00
DESKTOP-U2897F0\USER <aoel3040@gmail.com>
2021-04-22 11:16:00 +0900

    Module refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { SectionComponent } from './section/section.component';
-import { TimeDisplayComponent } from './section/time-display/time-display.component';
-import { ButtonsComponent } from './section/buttons/buttons.component';
 import { FormsModule } from '@angular/forms'
+import { LayoutModule } from './layout/layout.module';
+import { SectionModule } from './section/section.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    SectionComponent,
-    TimeDisplayComponent,
-    ButtonsComponent
   ],
   imports: [
     FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule,
+    SectionModule
+  ],
+  exports: [
+
   ],
   providers: [],
   bootstrap: [AppComponent]
